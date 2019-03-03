aa66b0a99ec306fd13fe9527c6ac69aa584c8978
Aturt2 <antoniomartinez222@hotmail.com>
2019-03-03 16:42:37 +0100

    Supervised Learning

---
+++ Supervised_Learning/Linear_regression/ejercicios_varios/oxygen/oxygen.R
@@ -0,0 +1,43 @@
+# Distillation experiment
+# Dependent variable (y) oxygen purity (%)
+# Independent variable (x) hydrocarbon level (%)
+
+rm(list=ls())
+
+x=c(.99,1.02,1.15,1.29,1.46,1.36,0.87,1.23,1.55,1.4,1.19,1.15,.98,1.01,1.11,1.2,1.26,1.32,1.43,.95)
+y=c(90.01,89.05,91.43,93.74,96.73,94.45,87.59,91.77,99.42,93.65,93.54,92.52,90.56,89.54,89.85,90.39,93.25,93.41,94.98,87.33)
+
+# The relation between the variables is almost linear
+
+plot(x,y,pch=3,xlab="HC Level", ylab="O Purity",col="red")
+
+# Estimators of regression parameters
+
+model=lm(y~x)
+
+model
+
+# Estimators of coefficients, tests and R2 coefficients
+
+summary(model)
+
+# Plot of the regression line
+plot(x,y,pch=3,xlab="HC Level", ylab="O Purity",col="red")
+abline(model)
+
+
+# Diagnostic plots 
+
+plot(fitted.values(model),residuals(model),col="red",pch=3)
+
+
+# Normality of error residuals
+
+qqnorm(residuals(model),col="red",pch=3)
+qqline(residuals(model))
+
+
+hist(y,prob=T)
+lines(density(y))
+
+
